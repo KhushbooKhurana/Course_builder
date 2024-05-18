dc89e7733bf82df43c33d050972dcb1e0b6278db
KhushbooKhurana <khurana14khushboo@gmail.com>
2024-05-18 15:23:59 +0530

    Create builder.js

---
+++ src/store/slices/builder.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const BuilderSlice = createSlice({
+  name: "builder",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = BuilderSlice.actions;
+
+export default BuilderSlice.reducer;
